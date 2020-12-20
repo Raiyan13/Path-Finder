dcf0dfb948f44fef2ea4d8e3a8367a9272750ef8
Hillol-Talukdar <talukdarhillol@gmail.com>
2020-12-20 23:54:51 +0600

    a_star_algorithm done. Hillol

--- a_star_algorithm.py
+++ a_star_algorithm.py
@@ -32,16 +32,35 @@ def algorithm(draw, grid, start, end):
 
 	while not priority_queue.empty():
 
-        #need to check event here
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
 
 		current = priority_queue.get()[2]
-        #add related code when start from 45
+		priority_queue_track.remove(current)
 
 		if current == end:
 			find_shortest_path(came_from, end, draw, start)
 			end.make_end()
 			return True
 
-        # need to check and traverse and update other things here
+		for neighbor in current.neighbors:
+			temp_g_cost = g_cost[current] + 1
 
+			if temp_g_cost < g_cost[neighbor]:
+				came_from[neighbor] = current
+				g_cost[neighbor] = temp_g_cost
+				f_cost[neighbor] = temp_g_cost + h(neighbor.get_current_position(), end.get_current_position())
+
+				if neighbor not in priority_queue_track:
+					cost += 1
+					priority_queue.put((f_cost[neighbor], cost, neighbor))
+					priority_queue_track.add(neighbor)
+					neighbor.make_open()
+
+		draw()
+
+		if current != start:
+			current.make_closed()
+			
 	return False
